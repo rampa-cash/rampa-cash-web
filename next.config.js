776364e45214cc3cd711559bae4deadd64ff4b58
Juan Betancur <jmbetan@icloud.com>
2025-05-02 16:32:51 +0200

    Comment out redirects in next.config.js to fix redirect loop issue

--- next.config.js
+++ next.config.js
@@ -2,44 +2,45 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-  async redirects() {
-    return [
-      // Redirect from www to non-www (or vice versa, depending on preference)
-      {
-        source: '/:path*',
-        has: [
-          {
-            type: 'host',
-            value: 'www.rampa.cash',
-          },
-        ],
-        destination: 'https://rampa.cash/:path*',
-        permanent: true,
-      },
-      {
-        source: '/:path*',
-        has: [
-          {
-            type: 'host',
-            value: 'www.rampacash.com',
-          },
-        ],
-        destination: 'https://rampa.cash/:path*',
-        permanent: true,
-      },
-      {
-        source: '/:path*',
-        has: [
-          {
-            type: 'host',
-            value: 'rampacash.com',
-          },
-        ],
-        destination: 'https://rampa.cash/:path*',
-        permanent: true,
-      },
-    ];
-  },
+  // Comment out the redirects
+  // async redirects() {
+  //   return [
+  //     // Redirect from www to non-www (or vice versa, depending on preference)
+  //     {
+  //       source: '/:path*',
+  //       has: [
+  //         {
+  //           type: 'host',
+  //           value: 'www.rampa.cash',
+  //         },
+  //       ],
+  //       destination: 'https://rampa.cash/:path*',
+  //       permanent: true,
+  //     },
+  //     {
+  //       source: '/:path*',
+  //       has: [
+  //         {
+  //           type: 'host',
+  //           value: 'www.rampacash.com',
+  //         },
+  //       ],
+  //       destination: 'https://rampa.cash/:path*',
+  //       permanent: true,
+  //     },
+  //     {
+  //       source: '/:path*',
+  //       has: [
+  //         {
+  //           type: 'host',
+  //           value: 'rampacash.com',
+  //         },
+  //       ],
+  //       destination: 'https://rampa.cash/:path*',
+  //       permanent: true,
+  //     },
+  //   ];
+  // },
 }
 
 module.exports = nextConfig
