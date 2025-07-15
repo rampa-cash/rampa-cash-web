e25ce5960ee77ed53cde2074b6695935a3c9f948
Steven Bustos <stvnbvsts@gmail.com>
2025-07-16 00:19:50 +0200

    fix to error in deployment

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,5 @@
 import type { AppProps } from 'next/app'
-import Layout from '../components/layout/layout'
+import Layout from '../components/layout/Layout'
 import '../styles/globals.css'
 
 const App = ({ Component, pageProps }: AppProps): JSX.Element => {
