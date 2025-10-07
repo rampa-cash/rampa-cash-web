ac8af0fb9264994143ffa897b39884db8cbc522e
Steven Bustos <stvnbvsts@gmail.com>
2025-10-08 00:45:31 +0200

    remove telemetry

--- features/auth/config/web3auth.config.ts
+++ features/auth/config/web3auth.config.ts
@@ -7,4 +7,6 @@ const clientId = process.env.NEXT_PUBLIC_WEB3AUTH_CLIENT_ID || '';
 export const web3AuthConfig: Web3AuthOptions = {
     clientId,
     web3AuthNetwork: WEB3AUTH_NETWORK.SAPPHIRE_DEVNET,
+    // Disable logging to reduce telemetry and analytics
+    enableLogging: false,
 };
