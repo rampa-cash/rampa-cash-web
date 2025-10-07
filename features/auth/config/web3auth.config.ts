4fdde14dd65ab4d2b6fe1eab9e7843f072642f5e
Steven Bustos <stvnbvsts@gmail.com>
2025-10-08 00:14:16 +0200

    remove login statement

--- features/auth/config/web3auth.config.ts
+++ features/auth/config/web3auth.config.ts
@@ -7,5 +7,4 @@ const clientId = process.env.NEXT_PUBLIC_WEB3AUTH_CLIENT_ID || '';
 export const web3AuthConfig: Web3AuthOptions = {
     clientId,
     web3AuthNetwork: WEB3AUTH_NETWORK.SAPPHIRE_DEVNET,
-    enableLogging: false,
 };
